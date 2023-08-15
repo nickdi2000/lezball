c472f9e80d2e58eb5bcfd52f0dccc4855faeb61c
nick di <nickdifelice@gmail.com>
2023-08-15 10:19:32 -0400

    initial commo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>
+					Welcome to <code>LezBall</code>.
+				</p>
+				<img src="https://cdn.vectorstock.com/i/preview-1x/37/42/piano-logo-graphic-design-template-vector-23103742.jpg" />
+			</header>
+		</div>
+	);
+}
+
+export default App;
